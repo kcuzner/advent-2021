c8b8c79da40a0012a1ec4eb45b483b7a1378dc5b
Kevin Cuzner <kevin@kevincuzner.com>
2021-12-14 21:08:30 -0600

    Puzzle 6 part 2

--- puzzle6/src/main.rs
+++ puzzle6/src/main.rs
@@ -2,11 +2,15 @@ use std::io::{self, BufRead};
 
 struct Lanternfish {
     counter: i32,
+    replicas: i64,
 }
 
 impl Lanternfish {
-    fn new(counter: i32) -> Self {
-        Self { counter: counter }
+    fn new(counter: i32, replicas: i64) -> Self {
+        Self {
+            counter: counter,
+            replicas: replicas,
+        }
     }
 
     /// Return true if a new lanternfish should spawn
@@ -29,18 +33,21 @@ fn main() {
         .next()
         .expect("No first line?")
         .expect("No string?");
+
     let mut fish: Vec<_> = init_state
         .split(",")
-        .map(|s| Lanternfish::new(s.parse::<i32>().expect("Invalid number")))
+        .map(|s| Lanternfish::new(s.parse::<i32>().expect("Invalid number"), 1))
         .collect::<Vec<_>>();
-    for _ in 0..80 {
-        let mut to_add: Vec<_> = Vec::<Lanternfish>::new();
+
+    for _ in 0..256 {
+        let mut replicas = 0;
         for f in fish.iter_mut() {
             if f.day() {
-                to_add.push(Lanternfish::new(8));
+                replicas += f.replicas;
             }
         }
-        fish.extend(to_add);
+        fish.push(Lanternfish::new(8, replicas));
     }
-    println!("Fish: {}", fish.len());
+    let total: i64 = fish.iter().map(|f| f.replicas).sum();
+    println!("Fish: {}", total);
 }
