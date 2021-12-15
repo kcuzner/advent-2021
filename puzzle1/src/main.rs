e090215b03e4972bd2ccdfd954d3aba9b5f1a812
Kevin Cuzner <kevin@kevincuzner.com>
2021-12-14 18:40:46 -0600

    Finished puzzle 1

---
+++ puzzle1/src/main.rs
@@ -0,0 +1,17 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut first = true;
+    let mut increased = 0;
+    let mut last = 0;
+    for line in stdin.lock().lines() {
+        let value = line.expect("No line?").parse::<u32>().expect("Invalid integer");
+        if value > last && !first {
+            increased += 1;
+        }
+        last = value;
+        first = false;
+    }
+    println!("Increased {} times", increased);
+}
